48f574bf4c8a3cbd443030d617f4903ce2dd522c
Youngwook Go <113055584+Youngwook-Go@users.noreply.github.com>
2023-01-16 19:19:32 +0000

    Initial commit

---
+++ main.cs
@@ -0,0 +1,33 @@
+using System;
+using System.Reflection;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using System.Net.Http;
+using System.Text.Json;
+using System.Text.Json.Nodes;
+
+class Program
+{
+    public static async Task Main()
+    {
+        HttpClient client = new HttpClient();
+        string response = await client.GetStringAsync(
+            "https://quotes.rest/bible/verse.json"
+        );
+        // Console.WriteLine(response);
+        var jsonAsDictionary = System.Text.Json.JsonSerializer.Deserialize<Object>(response);
+        // get content
+        Console.WriteLine("");
+        JsonNode bibleNode = JsonNode.Parse(response)!;
+        JsonNode contentNode = bibleNode!["contents"]!;
+        // contents
+        JsonNode verseNode = contentNode!["verse"]!;
+        JsonNode bookNode = contentNode!["book"]!;
+        JsonNode chapterNode = contentNode!["chapter"]!;
+        JsonNode numberNode = contentNode!["number"]!;
+        
+        Console.WriteLine(bookNode + " " + chapterNode + ":" + numberNode);
+        Console.WriteLine("\n" + verseNode);
+        Console.WriteLine("\ndone.");
+    }
+}
